f51e0eff84c0f81fbfb680dc7a748d009ba7bc20
davoler <david.cubillos@udea.edu.co>
2025-07-10 20:23:53 -0500

    Creación clase 'Viga' con método para mostrar información y calcular reacciones

---
+++ viga.py
@@ -0,0 +1,69 @@
+from cargapadre import Carga  # Importa la clase Carga desde el archivo carga.py
+from cargapuntual import CargaPuntual
+from cargarectangular import CargaDistribuidaRectangular
+from cargatriangular import CargaDistribuidaTriangular
+
+class Viga:
+    def __init__(self, longitud):
+        self.longitud = longitud #metros
+        self.cargas = [] #Se crea una lista vacía donde luego se guardarán las cargas.
+        
+    def agregar_carga(self, carga): #Método para agregar una carga a la viga
+        # if: Verifica si la carga está dentro de los límites de la viga
+        if hasattr(carga, 'posicion'):  # Para cargas puntuales
+            if carga.posicion < 0 or carga.posicion > self.longitud:
+                print("Error: la carga está fuera de la viga.")
+                return
+        elif hasattr(carga, 'inicio') and hasattr(carga, 'fin'):  # Para cargas distribuidas
+            if carga.inicio < 0 or carga.fin > self.longitud:
+                print("Error: la carga está fuera de la viga.")
+                return
+        self.cargas.append(carga) #Agrega la carga a la lista de cargas de la viga
+            
+    def mostrar_cargas(self): #Método para mostrar las cargas aplicadas a la viga
+        if not self.cargas:
+            print("No hay cargas aplicadas a la viga.")
+        else:
+            print("Cargas aplicadas:")
+            for carga in self.cargas:
+                carga.mostrar()
+
+    def calcular_reacciones(self):
+        reaccion_vertical = 0 #Inician en cero las reacciones en el empotramiento
+        momento_en_apoyo = 0
+
+        for carga in self.cargas:
+            if isinstance(carga, CargaPuntual):
+                F = carga.magnitud
+                x = carga.posicion
+
+            elif isinstance(carga, CargaDistribuidaRectangular):
+                base = carga.fin - carga.inicio
+                F = carga.intensidad * base
+                x = (carga.inicio + carga.fin) / 2
+
+            elif isinstance(carga, CargaDistribuidaTriangular):
+                base = carga.fin - carga.inicio
+                F = 0.5 * carga.intensidad_maxima * base
+                if carga.hacia_derecha:
+                    x = carga.inicio + (2 / 3) * base
+                else:
+                    x = carga.inicio + (1 / 3) * base
+
+            else:
+                print("Tipo de carga no reconocido.")
+                continue
+
+            reaccion_vertical += F
+            momento_en_apoyo += F * x
+
+        print(f"Reacción vertical en el empotramiento: {-reaccion_vertical} N")
+        #El signo negativo indica que la reacción es sentido contrario a la sumatoria de fuerzas.
+        #Usaremos por convención el signo negativo para indicar que la reacción es hacia abajo. Pasa
+        #lo mismo con el momento. El signo negativo indica que el momento es en sentido horario.
+        print(f"Momento en el empotramiento: {-momento_en_apoyo} N·m")
+
+
+
+
+
